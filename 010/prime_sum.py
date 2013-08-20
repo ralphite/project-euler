5d2bc153f88925676f9ae7d8325e31adf4202509
Yadong Wen <ralph.wen@gmail.com>
2013-08-19 18:08:22 -0700

    quick and dirty in py for 010. takes 4s to run

---
+++ 010/prime_sum.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys,math
+sys.path.append("../lib")
+from natural import Prime
+
+p = Prime()
+s=0
+
+while p.next()<math.sqrt(2000000):
+    pass
+pl = p.prime_list
+
+s= sum(pl)
+
+for i in range(pl[-1]+2,2000000,2):
+    is_prime = True
+    for j in pl:
+        if i%j==0:
+            is_prime=False
+            break
+    if is_prime:
+        s+=i
+
+print s
+
+
+
