fb23825e57330904b75abc01154288171ef11fd4
Yadong Wen <ralph.wen@gmail.com>
2013-08-19 17:25:45 -0700

    quick and dirty in py for 007. takes 3s to run

---
+++ 007/exec.sh
@@ -0,0 +1,1 @@
+time ./prime.py
---
+++ 007/prime.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import sys
+sys.path.append("../lib")
+from natural import Prime
+
+p = Prime()
+for i in range(1,10001):
+    p.next()
+print p.next()
